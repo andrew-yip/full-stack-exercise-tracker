bf039fb8cd9aa8a915ab517d637c1728bc2cca12
andrew-yip <andrew.yip22@gmail.com>
2022-10-15 21:42:31 -0700

    errors with server connectivity

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -30,8 +30,26 @@ export default class CreateUser extends Component {
 
         // sending to backend end point
         // expecting json body
-        axios.post('http://localhost:9000/users/add', user)
-            .then(res => console.log(res.data));
+        axios.post('http://localhost:9000/users/add', { user })
+            .then(res => console.log(res.data))
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log("Error.Request: ", error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+                console.log("Error.Config: ", error.config);
+            });
 
         this.setState({
             username: ''
